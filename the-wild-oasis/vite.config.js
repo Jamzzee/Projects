cd82dd4b3d8f18420916b48281104add302f0b96
Vitalii <vitaliichalbash@gmail.com>
2024-09-07 17:54:04 +0200

    added vercel.json and fixed vite.config

--- the-wild-oasis/vite.config.js
+++ the-wild-oasis/vite.config.js
@@ -5,4 +5,12 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint],
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Let Vite handle chunking
+      },
+    },
+  },
 });

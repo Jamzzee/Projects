d45d4b07bf6fe258eadfa3065dc280b526bbc738
Vitalii <vitaliichalbash@gmail.com>
2025-02-23 20:49:50 +0100

    Fixed auth.js

--- the-wild-oasis-fronted/app/_lib/auth.js
+++ the-wild-oasis-fronted/app/_lib/auth.js
@@ -2,9 +2,6 @@ import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 import { createGuest, getGuest } from './data-service';
 
-//* Set the correct URL (local or prod)
-const nextAuthUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000';
-
 const authConfig = {
   providers: [Google],
   callbacks: {
@@ -22,25 +19,6 @@ const authConfig = {
       } catch {
         return false;
       }
-    },
-    redirect: async ({ url, baseUrl }) => {
-      console.log('Redirecting from:', url);
-      console.log('Base URL:', baseUrl);
-
-      try {
-        const parsedUrl = new URL(url, baseUrl); // Ensures proper URL parsing
-        if (parsedUrl.pathname.startsWith('/account')) {
-          const finalRedirectUrl = `${nextAuthUrl.replace(/\/$/, '')}${
-            parsedUrl.pathname
-          }`;
-          console.log('SUCCESS: Redirecting to:', finalRedirectUrl);
-          return finalRedirectUrl;
-        }
-      } catch (error) {
-        console.error('Redirect Error:', error);
-      }
-
-      return baseUrl;
     },
     async session({ session, user }) {
       const guest = await getGuest(session.user.email);
